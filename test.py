fe167cca7808e598cb5a8ca3842270721a8276ea
Murielle123 <murielle.sanou1@gmail.com>
2024-01-31 15:36:38 +0100

    update test

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(1 + 2)
+# Importez la bibliothèque Streamlit
+import streamlit as st
+
+# Code Streamlit pour afficher "Hello"
+st.title("Hello Streamlit")
+st.write("Ceci est une application Streamlit de démonstration qui affiche 'Hello'.")
+
+# Exécutez l'application avec la commande streamlit run
+# Par exemple, sauvegardez ce script dans un fichier nommé app.py
+# Exécutez ensuite dans votre terminal : streamlit run app.py
